7fe04fd749794c0d98b3fb2e163372ccacc59844
kickscar <kickscar@gmail.com>
2016-10-20 09:39:30 +0900

    인증 여부 체크 추가

--- src/com/bit2016/mysite/action/board/ReplyFormAction.java
+++ src/com/bit2016/mysite/action/board/ReplyFormAction.java
@@ -5,9 +5,11 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.bit2016.mysite.dao.BoardDao;
 import com.bit2016.mysite.vo.BoardVo;
+import com.bit2016.mysite.vo.UserVo;
 import com.bit2016.web.Action;
 import com.bit2016.web.util.WebUtil;
 
@@ -15,6 +17,18 @@ public class ReplyFormAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// is auth?
+		HttpSession session = request.getSession();
+		if( session == null ) {
+			WebUtil.redirect(request, response, "/mysite3/board" );
+			return;
+		}
+		UserVo authUser = (UserVo)session.getAttribute( "authUser" );
+		if( authUser == null ) {
+			WebUtil.redirect(request, response, "/mysite3/board" );
+			return;
+		}
+		
 		long no = WebUtil.checkLongParam( request.getParameter( "no" ), 0L );
 		
 		BoardDao dao = new BoardDao();
